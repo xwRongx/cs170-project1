68bea450103a1e626117c2fc6fa2d94ff49059d5
Adreyan Distor <117056281+AdreyanDistor@users.noreply.github.com>
2024-04-29 14:37:55 -0700

    Update Display.cpp

--- src/Display.cpp
+++ src/Display.cpp
@@ -1,1 +1,27 @@
 #include "../headers/Display.h"
+#include "../headers/Node.h"
+#include "../headers/Problem.h"
+
+Display::Display()
+{}
+
+void Display::displayNode(Node* ptr)
+{
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            cout << ptr->board[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void Display::printSolutionPath(Node* ptr)
+{
+    if(ptr->parent == nullptr)
+    {
+        return;
+    }
+    printSolutionPath(ptr->parent);
+    displayNode(ptr);
+}
+
