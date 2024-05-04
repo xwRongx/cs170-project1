730bf312ea34e4efaf0fd803566a706e8cdc336c
Dacia Rios <71524653+DaciaRios@users.noreply.github.com>
2024-05-03 17:39:28 -0700

    Update Problem.h
    Fixing issue with findSpace functions.

--- headers/Problem.h
+++ headers/Problem.h
@@ -32,7 +32,7 @@ public:
     Node* moveDown(Node* state);
     Node* moveLeft(Node* state);
     Node* moveRight(Node* state);
-    pair<int, int>findSpace(); //Find 0 (empty space) in the 3 by 3 array to begin operators(up, down left, right) 
+    pair<int, int>findSpace(Node *state); //Find 0 (empty space) in the 3 by 3 array to begin operators(up, down left, right) 
     void display();
 
     ~Problem();
