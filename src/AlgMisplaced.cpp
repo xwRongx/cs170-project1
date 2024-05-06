bd60cd4ca4aacf14f6e9dc83148071a6d48b46de
Adreyan Distor <117056281+AdreyanDistor@users.noreply.github.com>
2024-05-05 20:20:02 -0700

    Update AlgMisplaced.cpp to use priority queues

--- src/AlgMisplaced.cpp
+++ src/AlgMisplaced.cpp
@@ -20,24 +20,14 @@ queue<Node *>* AlgMisplaced::queuingFunction(queue<Node*> *q1, queue<Node*> *q2)
         vec1.push_back(currNode);
     }
 
-    
-    int n = vec1.size();
-    for (int i = 0; i < n - 1; ++i) {
-        int minIndex = i;
-        for (int j = i + 1; j < n; ++j) {
-            if (vec1[j]->getFn() < vec1[minIndex]->getFn()) {
-                minIndex = j;
-            }
-        }
-        if (minIndex != i) {
-            swap(vec1[i], vec1[minIndex]);
-        }
+    priority_queue<Node*, vector<Node*>, CompareNodeFn> pq(vec1.begin(), vec1.end());
+
+    while (!pq.empty()) {
+        Node* currNode = pq.top();
+        mergedQueue->push(currNode);
+        pq.pop();
     }
 
-    for(int i = 0; i < vec1.size(); i++)
-    {
-        mergedQueue->push(vec1[i]);
-    }    
     return mergedQueue;
 }
 AlgMisplaced::AlgMisplaced() {
