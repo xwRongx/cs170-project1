315e3b50cb82fe625af463622260e72c27bb8a40
Adreyan Distor <117056281+AdreyanDistor@users.noreply.github.com>
2024-05-05 19:43:33 -0700

    Update AlgMisplaced.h SIZE

--- headers/AlgMisplaced.h
+++ headers/AlgMisplaced.h
@@ -10,14 +10,13 @@ using namespace std;
 
 class AlgMisplaced : public AlgGeneric {
 private:
-
+int goalState[SIZE][SIZE];
 int numberMisplacedTiles(Node* node)
 {
     int count = 0;
-    int goalState[3][3] = {{1,2,3}, {4,5,6},{7,8,0}};
-    for(int i = 0; i < 3; i++)
+    for(int i = 0; i < SIZE; i++)
     {
-        for(int j = 0; j < 3; j++)
+        for(int j = 0; j < SIZE; j++)
         {
             if(goalState[i][j] != node->getTile(i,j)) count++;
         }
